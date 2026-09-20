4ca24346a72c6dbd393200a8cc5caaf25e59baca
gabelaza000 <18385233+gabelaza000@users.noreply.github.com>
2026-09-19 22:51:45 -0500

    code solution

--- src/test/java/cs271/lab/list/TestPerformance.java
+++ src/test/java/cs271/lab/list/TestPerformance.java
@@ -16,11 +16,10 @@ public class TestPerformance {
   // comparing their running times for AddRemove vs. Access? Record those running times in README.txt!
   // TODO (optional) refactor to DRY
   // which of the two lists performs better as the size increases?
-  private final int SIZE = 10;
+  private final int SIZE = 10000;
 
-  // TODO choose this value in such a way that you can observe an actual effect
-  // for increasing problem sizes
-  private final int REPS = 1000000;
+  // chosen to make timings measurable without excessive runtime
+  private final int REPS = 1000;
 
   private List<Integer> arrayList;
 
